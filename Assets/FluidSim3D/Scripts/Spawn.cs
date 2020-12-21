367e0f83dedf9717fdfec3e742a17ffe91f3d267
Tasuku Takahashi <lightfox.task@gmail.com>
2020-12-21 21:03:28 +0900

    untracked files on develop: ce3373c Almost completed smoke shadering.

---
+++ Assets/FluidSim3D/Scripts/Spawn.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+using VoxelSystem;
+
+
+namespace FluidSim3DProject
+{
+
+    public class Spawn : MonoBehaviour {
+
+
+        [SerializeField] protected ComputeShader voxelizer;
+        public int numOfVoxels = 128;
+
+        protected Kernel setupKernel, updateKernel;
+        protected ComputeBuffer particleBuffer;
+
+        protected Renderer _renderer;
+        protected MaterialPropertyBlock block;
+
+        #region Shader property keys
+
+        protected const string kSetupKernelKey = "Setup", kUpdateKernelKey = "Update";
+
+        protected const string kVoxelBufferKey = "_VoxelBuffer", kVoxelCountKey = "_VoxelCount";
+        protected const string kParticleBufferKey = "_ParticleBuffer", kParticleCountKey = "_ParticleCount";
+        protected const string kUnitLengthKey = "_UnitLength";
+
+        #endregion
+
+        protected GPUVoxelData gpuVoxelData;
+
+        void InitSpawn () {
+            var mesh = SampleMesh();
+            if (mesh == null) return;
+            gpuVoxelData = GPUVoxelizer.Voxelize(voxelizer, mesh, numOfVoxels, true);
+        }
+        
+        void UpdateSpawn () {
+            if (gpuVoxelData == null) return;
+
+            gpuVoxelData.Dispose();
+
+            var mesh = SampleMesh();
+            if (mesh == null) return;
+            gpuVoxelData = GPUVoxelizer.Voxelize(voxelizer, mesh, numOfVoxels, true);
+        }
+
+        public GPUVoxelData GetGPUVoxelData() {
+            return this.gpuVoxelData;
+        }
+
+        Mesh SampleMesh()
+        {
+            if (this.GetComponent<MeshRenderer>() != null) {
+                return this.GetComponent<MeshFilter>().mesh;
+            }
+            else if (this.GetComponent<SkinnedMeshRenderer>() != null) {
+                var mesh = new Mesh();
+                this.GetComponent<SkinnedMeshRenderer>().BakeMesh(mesh);
+                return mesh;
+            } else {
+                Debug.LogError("Attach MeshRenderer or SkinnedMeshRenderer on a Spawn Object");
+                return null;
+            }
+        }
+
+        void OnDestroy ()
+        {
+            if(gpuVoxelData != null)
+            {
+                gpuVoxelData.Dispose();
+                gpuVoxelData = null;
+            }
+        }
+
+    }
+
+}
+
---
+++ Assets/FluidSim3D/Shaders/rgb_to_cmyk.hlsl
@@ -0,0 +1,27 @@
+float3 CMYKtoRGB (float4 cmyk) {
+    float c = cmyk.x;
+    float m = cmyk.y;
+    float y = cmyk.z;
+    float k = cmyk.w;
+
+    float invK = 1.0 - k;
+    float r = 1.0 - min(1.0, c * invK + k);
+    float g = 1.0 - min(1.0, m * invK + k);
+    float b = 1.0 - min(1.0, y * invK + k);
+    return clamp(float3(r, g, b), 0.0, 1.0);
+}
+
+float4 RGBtoCMYK (float3 rgb) {
+    float r = rgb.r;
+    float g = rgb.g;
+    float b = rgb.b;
+    float k = min(1.0 - r, min(1.0 - g, 1.0 - b));
+    float3 cmy = float3(0, 0, 0);
+    float invK = 1.0 - k;
+    if (invK != 0.0) {
+        cmy.x = (1.0 - r - k) / invK;
+        cmy.y = (1.0 - g - k) / invK;
+        cmy.z = (1.0 - b - k) / invK;
+    }
+    return clamp(float4(cmy, k), 0.0, 1.0);
+}
